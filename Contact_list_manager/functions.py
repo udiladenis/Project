d3f15db2247b7a9772e1d725e67a1e37541c0e00
udiladenis <101632262+udiladenis@users.noreply.github.com>
2022-03-31 17:19:51 +0300

    Update functions.py

--- Contact_list_manager/functions.py
+++ Contact_list_manager/functions.py
@@ -1,5 +1,6 @@
 import json
 
+# Function that verify if the every element of phone number is digit and if the lenght of the number is == 10
 def verify_phone_number(phone_number):
     for digit in phone_number:
         if not digit.isdigit():
@@ -7,12 +8,12 @@ def verify_phone_number(phone_number):
 
     return len(phone_number) == 10
 
-
+# Verify email function
 def verify_email_address(email):
-    if "@" not in email:
+    if "@" not in email: # Check if the "@" element is in the "email"
         return False
 
-    split_email = email.split("@")
+    split_email = email.split("@") # split the "email"
     identifier = "".join(split_email[:-1])
     domain = split_email[-1]
 
@@ -30,8 +31,7 @@ def verify_email_address(email):
 
     return True
 
-
-
+# Function that check if in the contact object exist the contacts based on the first and last name parameters
 def get_contact_by_name(first_name, last_name, contacts):
     for contact in contacts:
         if contact["First Name"] == first_name and contact["Last Name"] == last_name:
@@ -39,7 +39,7 @@ def get_contact_by_name(first_name, last_name, contacts):
 
     return None
 
-
+# Read contact file based on path that we provide
 def read_contacts(file_path):
     try:
         with open(file_path, 'r') as f:
@@ -49,7 +49,7 @@ def read_contacts(file_path):
 
     return contacts
 
-
+# Write new contact in our JSON file
 def write_contacts(file_path, contacts):
     with open(file_path, 'w') as f:
         contacts = {"contacts": contacts}
